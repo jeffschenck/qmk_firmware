5e238d2a722c77a5cad9b554e05ad1bc554bc2e4
Jeff Schenck <jmschenck@gmail.com>
2016-02-01 22:31:03 -0800

    Map F23 and F24 for brightness control

--- keyboard/ergodox_ez/keymaps/jeffschenck/keymap.c
+++ keyboard/ergodox_ez/keymaps/jeffschenck/keymap.c
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |GiAtUp|      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | BrDn | BrUp |CtrlLt|CtrlRt|      |------|           |------|      |GiAtLt|GiAtDn|GiAtRt|      |        |  <-- KC_SLCK and KC_PAUS are brightness down/up in recent OSX
+ * |        | BrDn | BrUp |CtrlLt|CtrlRt|      |------|           |------|      |GiAtLt|GiAtDn|GiAtRt|      |        |  <-- KC_23 and KC_25 are mapped in software to brightness down/up
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |GuiTab|GiStTb|      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Left Hand
   KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,
   KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,
-  KC_TRNS,          KC_SLCK,          KC_PAUS,          LCTL(KC_LEFT),    LCTL(KC_RGHT),    KC_TRNS,
+  KC_TRNS,          KC_F23,           KC_F24,           LCTL(KC_LEFT),    LCTL(KC_RGHT),    KC_TRNS,
   KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,
   KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,
 
