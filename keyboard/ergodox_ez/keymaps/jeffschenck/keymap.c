f67cc7826a774aa98073e26483061e27b5d6bc05
Jeff Schenck <jmschenck@gmail.com>
2016-02-13 12:52:29 -0800

    Add extra 5/6 keys to top row

--- keyboard/ergodox_ez/keymaps/jeffschenck/keymap.c
+++ keyboard/ergodox_ez/keymaps/jeffschenck/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | `      |   1  |   2  |   3  |   4  |   5  |~~~~~~|           |~~~~~~|   6  |   7  |   8  |   9  |   0  | -      |
+ * | `      |   1  |   2  |   3  |   4  |   5  |   6  |           |   5  |   6  |   7  |   8  |   9  |   0  | -      |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |~~~~~~|           |~~~~~~|   Y  |   U  |   I  |   O  |   P  | Meh    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = KEYMAP(
   // Left Hand
-  KC_GRV,           KC_1,             KC_2,             KC_3,             KC_4,             KC_5,             KC_NO,
+  KC_GRV,           KC_1,             KC_2,             KC_3,             KC_4,             KC_5,             KC_6,
   KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_NO,
   CTL_T(KC_LCAP),   LT(PROG,KC_A),    KC_S,             KC_D,             KC_F,             KC_G,
   KC_LSFT,          KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_NO,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_ENT,           KC_LGUI,          KC_RPRN,
 
   // Right Hand
-  KC_NO,            KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             KC_MINS,
+  KC_5,             KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             KC_MINS,
   KC_NO,            KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             MEH_T(KC_NO),
                     KC_H,             KC_J,             KC_K,             KC_L,             LT(MDIA,KC_SCLN), ALL_T(KC_QUOT),
   KC_NO,            KC_N,             KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          KC_RSFT,
